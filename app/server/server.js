3966c11c6b2ff1f9c5b0085654abb168ee07c853
ng-tony <tonyn.20500@gmail.com>
2017-07-03 21:31:41 -0400

    fixed bug

--- app/server/server.js
+++ app/server/server.js
@@ -183,7 +183,7 @@ app.get("/getFriends", function(req, res) {
 app.get("/games/top/rating", function(req, res) {
 	console.log("getting top games");
 	var limit = req.query.limit;
-	var selectSQL = 'select * from games join Ratings on games.Title = Ratings.Title order by "round(avg(rating),1)" desc limit ?';
+	var selectSQL = 'select * from games join Ratings on games.Title = Ratings.Title order by "rating" desc limit ?';
 	db.all(selectSQL, limit, function(err, rows){
 		res.send(rows);
 	});
