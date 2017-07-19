f317665cc03b79e6610523109383e0252b3959ce
shuyi56 <shuyi56@gmail.com>
2017-07-18 21:49:44 -0400

    i think this is the fix

--- app/web_pages/navBar.js
+++ app/web_pages/navBar.js
@@ -188,6 +188,8 @@ Vue.component("navbar", {
     logOut: function() {
       this.signedIn = false;
       sessionStorage.setItem("signedIn", false);
+      window.location.href =
+        "https://utsccscc01.github.io/final-project-team-8/app/web_pages/main_page.html";
     }
   }
 });
