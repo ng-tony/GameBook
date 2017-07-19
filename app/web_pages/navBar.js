bebb216fa506fc17d79bb2319d08f6e0a310aabd
shuyi56 <shuyi56@gmail.com>
2017-07-18 21:46:35 -0400

    attempt 2

--- app/web_pages/navBar.js
+++ app/web_pages/navBar.js
@@ -188,8 +188,6 @@ Vue.component("navbar", {
     logOut: function() {
       this.signedIn = false;
       sessionStorage.setItem("signedIn", false);
-      window.location.href =
-        "https://utsccscc01.github.io/final-project-team-8/app/web_pages/main_page.html";
     }
   }
 });
